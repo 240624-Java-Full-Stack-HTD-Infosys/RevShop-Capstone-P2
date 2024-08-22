bef58fc921f2ce1e48ac32dde8a375b30bea9ef6
Finw7 <finwall26@gmail.com>
2024-08-22 12:07:20 -0400

    Repository update

--- server/RevShop/src/main/java/net/revature/RevShop/Repositories/CartRepository.java
+++ server/RevShop/src/main/java/net/revature/RevShop/Repositories/CartRepository.java
@@ -4,13 +4,14 @@ import net.revature.RevShop.Models.CartItem;
 import net.revature.RevShop.Models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 import java.util.List;
 
 @Repository
 public interface CartRepository extends JpaRepository<CartItem, Integer> {
     List<CartItem> findByBuyer(User buyer);
-    CartItem findByCartItemId(Integer cartItemId);
+    Optional<CartItem> findByCartItemId(Integer cartItemId);
 
     void deleteByCartItemId(Integer cartItemId);
 }
